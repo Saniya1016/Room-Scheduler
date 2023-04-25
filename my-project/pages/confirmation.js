266f55cb3ba0bf992b20cc647bb643bad0a87bfe
Aadit <aadit.bhatia93@gmail.com>
2023-04-24 21:06:22 -0400

    Finished Confirmation page functionality

--- my-project/pages/confirmation.js
+++ my-project/pages/confirmation.js
@@ -1,6 +1,22 @@
 const logout_button = document.getElementById("log_out");
-
+const loginButton = document.getElementById("login_page");
+const homeButton = document.getElementById("home_page");
+const roomButton = document.getElementById("rooms_page");
+const chooseButton = document.getElementById("choose_page")
 
 logout_button.addEventListener("click", ()=>{
     window.location.href = "home.html"
+})
+
+loginButton.addEventListener("click", ()=>{
+    window.location.href = "login.html"
+})
+homeButton.addEventListener("click", ()=>{
+    window.location.href = "home.html"
+})
+roomButton.addEventListener("click", ()=>{
+    window.location.href = "rooms.html"
+})
+chooseButton.addEventListener("click", ()=>{
+    window.location.href = "choose_action.html"
 })
