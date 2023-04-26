32722fc1452a98e966160f16051e6c2d992c3810
Aadit <aadit.bhatia93@gmail.com>
2023-04-25 23:22:19 -0400

    Calendar mostly done- couple of issues with date indices

--- my-project/pages/calendar.js
+++ my-project/pages/calendar.js
@@ -12,6 +12,7 @@ const currentDate = new Date();
 let date = currentDate;
 let day = currentDate.getDay();
 let month = currentDate.getMonth() + 1;
+let year = currentDate.getFullYear();
 //For navbar
 
 loginButton.addEventListener("click", ()=>{
@@ -30,30 +31,63 @@ chooseButton.addEventListener("click", ()=>{
 
 // Assigning month
 monthElement.innerHTML = numToMonth[month];
-let year = currentDate.getFullYear();
-
+const clearDates =() =>{
+    for(let i = 1; i<31; ++i){
+        let currBut = document.getElementById("day"+i.toString());
+        currBut.innerHTML = "";
+    }
+}
+function daysInMonth (month, year) {
+    return new Date(year, month, 0).getDate();
+}
+const assignDates = () =>{
+    const firstDay = new Date(year, month-1, 1);
+    let nameOfDay = firstDay.getDay();
+    let numDaysInMonth= daysInMonth(month-1, year);
+    let count = 0
+    const today = new Date();
+    for(let i = 1; i<39; ++i){
+        let currBut = document.getElementById("day"+i.toString());
+        if(i<nameOfDay || numDaysInMonth===0){
+            count = count+1
+        }
+        else{
+            currBut.innerHTML = i-count;
+            if(i-count === today.getDay() && month-1 === today.getMonth()){
+                currBut.setAttribute('role', 'link');
+                currBut.setAttribute('tabindex', '0');
+                currBut.classList.add('focus:outline-none', 'focus:ring-2', 'focus:ring-offset-2', 'focus:ring-indigo-700', 'focus:bg-indigo-500', 'hover:bg-indigo-500', 'text-base', 'w-8', 'h-8', 'flex', 'items-center', 'justify-center', 'font-medium', 'text-white', 'bg-indigo-700', 'rounded-full');
+             //   <a  role="link" tabindex="0" class="focus:outline-none  focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:bg-indigo-500 hover:bg-indigo-500 text-base w-8 h-8 flex items-center justify-center font-medium text-white bg-indigo-700 rounded-full"></a>
+            }
+            numDaysInMonth = numDaysInMonth - 1;
+        }
+    }
+}
+assignDates();
 
 backButton.addEventListener("click", ()=>{
     month = month - 1;
     if(month===0){
         month = 12
+        year = year - 1;
     }
     monthElement.innerHTML = numToMonth[month];
+    clearDates();
+    assignDates();
 })
 forwardButton.addEventListener("click", ()=>{
     month = month + 1;
     if(month===13){
         month = 1
+        year = year+1
     }
     monthElement.innerHTML = numToMonth[month];
+    clearDates();
+    assignDates();
 })
 
-const changeDayToToday = document.getElementById("changeDayToToday");
-changeDayToToday.addEventListener("click", ()=>{
-    todayElement.month = todayElement.month;
-})
+ // create a new Date object with the first day of the current month and year
 
-const changeDayForward = document.getElementById("changeDayForward");
-changeDayForward.addEventListener("click", ()=>{
-    todayElement.month = todayElement.month+1;
-})
+
+
+
